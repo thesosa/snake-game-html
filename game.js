aa441b8a36ead560a82f8a430bb6c3dc250e8d9f
David Sosa <davidjsosav@gmail.com>
2022-08-28 11:38:32 +0300

    add license and copyright notice

--- game.js
+++ game.js
@@ -1,3 +1,21 @@
+/*
+    Classic Snake Game Html and Vanilla JavaScript
+    Copyright (C) 2022  David Sosa
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // render context
 const canv = document.getElementById('game-canvas');
 const ctx = canv.getContext('2d');
